1bbfa11f34e388d6107befa55702254e6c46ddb8
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-03-09 10:52:39 +0100

    Updates

---
+++ precache-manifest.79152eadb39adaf768182f0dadf91070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8de68b2e43aa93a73d6dec180b4122c",
+    "url": "/mern-rest-api/index.html"
+  },
+  {
+    "revision": "cab162113b5d5f0d44ac",
+    "url": "/mern-rest-api/static/css/main.5f832086.chunk.css"
+  },
+  {
+    "revision": "ee975fa2841cd5dd8d24",
+    "url": "/mern-rest-api/static/js/2.5ee425e9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mern-rest-api/static/js/2.5ee425e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab162113b5d5f0d44ac",
+    "url": "/mern-rest-api/static/js/main.bd63249f.chunk.js"
+  },
+  {
+    "revision": "45681dcdc6de0822f230",
+    "url": "/mern-rest-api/static/js/runtime-main.24f97a70.js"
+  }
+]);
